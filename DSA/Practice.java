9b461fdeeb1b0e9015d6cda43ff2d33b8653064c
Omkar Date <omkardate29@gmail.com>
2023-01-29 23:58:27 +0530

    Don't know what to commentgit status!

--- DSA/Practice.java
+++ DSA/Practice.java
@@ -1,11 +1,18 @@
-import java.util.Scanner;
-
 public class Practice {
   static void printArray(int arr[]) {
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
     }
     System.out.println();
+  }
+
+  static void printArray(int arr[][]) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 
   static int[] swap(int arr[], int i, int j) {
@@ -22,27 +29,67 @@ public class Practice {
     return arr;
   }
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    int[] arr = { 2, 1, 3, 4, 5 };
-
-    int start = 0;
-    int end = arr.length - 1;
-
-    for (int i = 1; i <= end; i++) {
-      arr[i] += arr[i - 1];
+  static int[][] transpose(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr[i].length; j++) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+      }
     }
+    return arr;
+  }
 
-    // while (start <= end) {
-    // if (Math.abs(arr[start]) > Math.abs(arr[end]))
-    // ans[j++] = arr[start] * arr[start++];
-    // else
-    // ans[j++] = arr[end] * arr[end--];
-    // }
+  static int[][] pascalTriangle(int n) {
+    int arr[][] = new int[n][];
+    for (int i = 0; i < n; i++) {
+      arr[i] = new int[i + 1];
+      arr[i][0] = arr[i][i] = 1;
 
-    // reverse(ans, 0, ans.length - 1);
+      for (int j = 1; j < i; j++) {
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+      }
+    }
+    return arr;
+  }
 
-    printArray(arr);
+  public static void main(String[] args) {
+    int[][] arr = {
+        { 1, 2, 3, 4 },
+        { 1, 2, 3, 4 },
+        { 1, 2, 3, 4 },
+    };
+
+    int rowStart = 0;
+    int rowEnd = arr[0].length - 1;
+    int colStart = 0;
+    int colEnd = arr.length - 1;
+
+    while (rowStart <= rowEnd && colStart <= colEnd) {
+      for (int row = rowStart; row <= rowEnd; row++) {
+        // System.out.print(arr[rowStart][row] + " ");
+        System.out.println("(" + rowStart + " " + row + ")");
+      }
+
+      colStart++;
+      for (int col = colStart; col <= colEnd; col++) {
+        // System.out.print(arr[col][rowEnd] + " ");
+        System.out.println("(" + col + " " + rowEnd + ")");
+      }
+
+      rowEnd--;
+      for (int row = rowEnd; row >= rowStart; row--) {
+        // System.out.print(arr[colEnd][row] + " ");
+        System.out.println("(" + colEnd + " " + row + ")");
+      }
+
+      colEnd--;
+      for (int col = colEnd; col >= colStart; col--) {
+        // System.out.print(arr[col][rowStart] + " ");
+        System.out.println("(" + col + " " + rowStart + ")");
+      }
+
+      rowStart++;
+    }
   }
 }
--- web_dev/Front-End/Practice/index.js
+++
@@ -1,5 +0,0 @@
-let x = "Omkar";
-{
-  let x = 23;
-}
-console.log(x);
