7fcea4658a382a7ed814204913fae705cfc1a854
Omkar Date <omkardate29@gmail.com>
2023-08-02 23:07:40 +0530

    some changes in node + typescript environment setup

--- web_dev/typescript-node/src/app.ts
+++ web_dev/typescript-node/src/app.ts
@@ -1,9 +1,17 @@
+import 'dotenv/config';
 import express, { Express } from 'express';
 
 const app: Express = express();
 
-app.get('/', (req, res) => {
-  res.redirect('/api-docs');
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
