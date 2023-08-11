a381cd08aa2928de6abde7b1796954d3313bc234
Omkar Date <omkardate29@gmail.com>
2023-08-12 01:18:47 +0530

    incomplete projects gone at their place

--- MERN-Auth-Basic/frontend/src/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const NotFound = () => {
-  const { params } = useParams();
-  return <div>{`cannot get /${params}`}</div>;
-};
-export default NotFound;
